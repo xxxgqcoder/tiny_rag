17be80aab149afea58f2d98d62f7c4baa49fedce
xxxgqcoder <xxxgqcoder@gmail.com>
2025-05-15 15:12:15 +0800

    rename file

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,13 @@ echo "process started"
 sh start_jupyter.sh
 echo 'jupyter server started'
 
+
+# replace project root directory in MinerU config
+sed 
+echo "finish replace project root directory"
+
 tail -f /dev/null
 
 wait
+
+
