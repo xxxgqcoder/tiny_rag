11364fe3f30525e53bd1dbaaade1ea211f2e4bdf
xxxgqcoder <xxxgqcoder@gmail.com>
2025-05-16 08:46:20 +0800

    add content resources url

--- parse/parser.py
+++ parse/parser.py
@@ -24,18 +24,22 @@ class Chunk:
         content_type: ChunkType,
         content: bytes,
         extra_description: bytes,
+        content_url: str='',
     ):
         """
         Args:
         - content_type: chunk content type.
         - content: the content, represented in bytes.
         - extra_description: content extra description.
+        - content_url: url to the content, set when content is not suitable for
+            directly insert into db, for example image / audio data.
         """
         super().__init__()
 
         self.content_type = content_type
         self.content = content
         self.extra_description = extra_description
+        self.content_url = content_url
         self.uuid = xxhash.xxh64(content + extra_description).hexdigest()
 
     def __str__(self, ):
