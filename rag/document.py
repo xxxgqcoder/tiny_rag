0839594b2ecd4f9f8e37f980d47a0c32d4503844
xxxgqcoder <xxxgqcoder@gmail.com>
2025-05-23 15:47:15 +0800

    new file process logic

--- rag/document.py
+++ rag/document.py
@@ -3,18 +3,27 @@ import sys
 import argparse
 import traceback
 import logging
+import os
 from typing import Union, Dict, List, Any
 
 import config
 from .nlp import EmbeddingModel
 from parse.parser import Chunk
+from utils import now_in_utc
+from rag.db import get_vector_db, get_rational_db
+from rag.nlp import get_embed_model
 
 
-def make_record(chunk: Chunk, embed: EmbeddingModel) -> Dict[str, Any]:
+def make_record(
+    file_path: str,
+    chunk: Chunk,
+    embed: EmbeddingModel,
+) -> Dict[str, Any]:
     """
     Make a record from chunk.
 
     Args:
+    - file_path: original file path.
     - chunk: parsed chunk.
     - embed: embedding model.
 
@@ -26,7 +35,7 @@ def make_record(chunk: Chunk, embed: EmbeddingModel) -> Dict[str, Any]:
         content = chunk.extra_description
     content = content.decode('utf-8')
 
-    meta = {}
+    meta = {'file_path': file_path}
     if chunk.content_type == config.ChunkType.IMAGE:
         meta['content_url'] = chunk.content_url
     if chunk.content_type == config.ChunkType.TABLE:
@@ -43,7 +52,7 @@ def make_record(chunk: Chunk, embed: EmbeddingModel) -> Dict[str, Any]:
     }
 
 
-def process_file(file_path: str) -> Dict[str, bool]:
+def process_new_file(file_path: str) -> Dict[str, bool]:
     """
     Process a file, parse and save chunks into db.
 
@@ -55,20 +64,24 @@ def process_file(file_path: str) -> Dict[str, bool]:
         with the order of chunks in original file.
     """
     from parse.pdf_parser import PDFParser
-    from rag.db import get_vector_db
-    from rag.nlp import get_embed_model
+    from config import PARSED_ASSET_DATA_DIR
 
-    from config import PARSED_ASSET_DATA_DIR, MILVUS_COLLECTION_NAME, MILVUS_DB_NAME
     parser = PDFParser()
 
     vector_db = get_vector_db()
+    sql_db = get_rational_db()
     embed_model = get_embed_model()
 
-    # parse file
-    chunks = parser.parse(file_path=file_path,
-                          asset_save_dir=PARSED_ASSET_DATA_DIR)
+    # clean up document
+    clean_up_document(file_path=file_path)
 
-    # insert into vector db
+    # parse file
+    chunks = parser.parse(
+        file_path=file_path,
+        asset_save_dir=PARSED_ASSET_DATA_DIR,
+    )
+
+    # insert parsed chunks into vector db
     records = [make_record(chunk, embed_model) for chunk in chunks]
     logging.info(f'total {len(records)} records')
 
@@ -76,8 +89,12 @@ def process_file(file_path: str) -> Dict[str, bool]:
     success_records = {}
     for record in records:
         try:
-            vector_db.insert(record)
-            success_records[record['uuid']] = True
+            insert_cnt = vector_db.insert(record)
+            if insert_cnt == 1:
+                success_records[record['uuid']] = True
+            else:
+                failed_records.append(record)
+
         except Exception as e:
             logging.info(f"Exception: {type(e).__name__} - {e}")
 
@@ -86,23 +103,67 @@ def process_file(file_path: str) -> Dict[str, bool]:
 
             failed_records.append(record)
 
-    # second try if any failure
     for record in failed_records:
         try:
-            vector_db.insert(record)
-            success_records[record['uuid']] = True
+            insert_cnt = vector_db.insert(record)
+            if insert_cnt == 1:
+                success_records[record['uuid']] = True
         except Exception as e:
             logging.info(f"Exception: {type(e).__name__} - {e}")
 
             formatted_traceback = traceback.format_exc()
             logging.info(formatted_traceback)
 
-    logging.info(f'successfully insert {len(success_records)} records')
-
-    # update file - chunking state
-    # TODO
-
-    return [
+    logging.info(
+        f'successfully insert {len(success_records)} records into vector db')
+    inserted_chunks = [
         record['uuid'] for record in records
         if record['uuid'] in success_records
     ]
+
+    # update document record
+    document_record = {
+        'name': os.path.basename(file_path),
+        'chunks': '\x07'.join(inserted_chunks),
+        'created_date': now_in_utc(),
+    }
+    insert_cnt = sql_db.insert_document(document_record)
+    if insert_cnt < 1:
+        logging.info(f'fail to insert document: {file_path}, retrying...')
+        sql_db.insert_document(document_record)
+
+    return inserted_chunks
+
+
+def clean_up_document(file_path: str):
+    """
+    Clean up document records.
+    
+    Args:
+    - file_path: path to the file.
+    """
+    vector_db = get_vector_db()
+    sql_db = get_rational_db()
+
+    file_name = os.path.basename(file_path)
+
+    # get document record
+    document_record = sql_db.get_document(file_name)
+    if document_record is None or len(document_record):
+        logging.info(f'document record {file_name} not found, ignore')
+        return
+
+    # delete document record
+    sql_db.delete_document(name=file_name)
+
+    # delete chunks
+    uuids = []
+    if 'chunks' in document_record and len(document_record['chunks']) > 0:
+        uuids = document_record['chunks'].split('\x07')
+    logging.info(f'total {len(uuids)} chunks')
+
+    total_delete_cnt = 0
+    for uuid in uuids:
+        delete_cnt = vector_db.delete(key=uuid)
+        total_delete_cnt += delete_cnt
+    logging.info(f'delete {total_delete_cnt} chunks from vector db')
