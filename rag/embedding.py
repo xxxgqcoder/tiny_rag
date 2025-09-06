8d1a14c005ed90934708210e2cc3dd2c2a172d85
xxxgqcoder <xxxgqcoder@gmail.com>
2025-09-06 13:01:58 +0800

    embedding

---
+++ rag/embedding.py
@@ -0,0 +1,44 @@
+
+from abc import ABC, abstractmethod
+from typing import Any
+
+import numpy as np
+from sentence_transformers import SentenceTransformer
+from common.utils import singleton, time_it
+from common.config import TinyRAGConfig
+
+
+class EmbeddingModel(ABC):
+    def __init__(self, name):
+        super().__init__()
+        self.name = name
+
+    @abstractmethod
+    def encode(self, texts: list[str], **kwargs) -> dict[str, Any]:
+        """
+        Encode text as vector. Some model is versatile and can return both dense and sparse vector.
+
+        Args:
+        - texts: the texts to encode.
+
+        Returns:
+        - Encoded vector, represented by a dict. Key is the encoded vector type, i.e., `dense`, `sparse`. Value is the encoded vector value.
+        """
+        raise NotImplementedError("Not implemented")
+
+
+@singleton
+class Qwen3Embedding(EmbeddingModel):
+    def __init__(self, model_dir: str):
+        self.model_dir = model_dir
+        self.model = SentenceTransformer(model_dir)
+
+    @time_it(prefix="Qwen3 ebmedding")
+    def encode(self, texts: list[str], **kwargs) -> dict[str, Any]:
+        prompt_name = kwargs.get("prompt_name", None)
+        embeddings = self.model.encode(texts, prompt_name=prompt_name)
+        return {"dense": embeddings}
+
+
+def get_embedding_model() -> EmbeddingModel:
+    return Qwen3Embedding(model_dir=TinyRAGConfig.embedding_config.embedding_model_dir)  # type: ignore
