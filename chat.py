36be9f08a9a8434b5be3c589d8d714f6231321de
xxxgqcoder <xxxgqcoder@gmail.com>
2025-09-06 16:56:53 +0800

    update

--- chat.py
+++ chat.py
@@ -1,18 +1,14 @@
-import sys
 import json
-import time
-import datetime
 import os
-import re
-from typing import Dict, Any
+import time
 from concurrent.futures import ThreadPoolExecutor
 
+import config
 import requests
 from prompt_toolkit import prompt
 from strenum import StrEnum
+from utils import logging_exception
 
-import config
-from utils import logging_exception, singleton
 from rag.llm import format_reference_info
 
 
@@ -25,8 +21,8 @@ class Role(StrEnum):
 # current ongoing conversation
 conversation = {}
 
-chat_server_url = 'http://127.0.0.1:4567/chat_completion'
-input_placeholder = '<type <esc> then <enter> to finish input>'
+chat_server_url = "http://127.0.0.1:4567/chat_completion"
+input_placeholder = "<type <esc> then <enter> to finish input>"
 
 job_executor = None
 is_generating = False
@@ -45,10 +41,13 @@ def generate_response() -> requests.models.Response:
     global conversation, is_generating
 
     reduced_conv = {}
-    reduced_conv['history'] = [{
-        'role': history['role'],
-        'content': history['content'],
-    } for history in conversation['history']]
+    reduced_conv["history"] = [
+        {
+            "role": history["role"],
+            "content": history["content"],
+        }
+        for history in conversation["history"]
+    ]
 
     is_generating = True
     response = requests.post(
@@ -61,7 +60,7 @@ def generate_response() -> requests.models.Response:
     response.raise_for_status()
 
     # print response
-    print('', end='\r', flush=True)
+    print("", end="\r", flush=True)
 
     last_ans = ""
     json_buffer = ""
@@ -71,30 +70,30 @@ def generate_response() -> requests.models.Response:
     answer_token_num = 0
     try:
         for chunk in response.iter_content(
-                chunk_size=8192,
-                decode_unicode=True,
+            chunk_size=8192,
+            decode_unicode=True,
         ):
             if not chunk:
                 continue
             json_buffer += chunk
             try:
                 ret = json.loads(json_buffer)
-                data = ret.get('data', {})
+                data = ret.get("data", {})
                 if len(data) == 0:
                     break
 
-                answer_token_num = data['answer_token_num']
-                prompt_token_num = data['prompt_token_num']
+                answer_token_num = data["answer_token_num"]
+                prompt_token_num = data["prompt_token_num"]
 
                 if not context_prompt:
-                    context_prompt = data['prompt']
+                    context_prompt = data["prompt"]
                 if not reference_meta:
-                    reference_meta = data['reference_meta']
+                    reference_meta = data["reference_meta"]
 
-                print(data['answer'][len(last_ans):], end='', flush=True)
+                print(data["answer"][len(last_ans) :], end="", flush=True)
 
                 json_buffer = ""
-                last_ans = data['answer']
+                last_ans = data["answer"]
             except json.JSONDecodeError:
                 continue
     except Exception as e:
@@ -104,27 +103,29 @@ def generate_response() -> requests.models.Response:
     formatted_reference_info = format_reference_info(reference_meta, last_ans)
     print(formatted_reference_info)
 
-    conversation['history'].append({
-        'role': 'assistant',
-        'content': last_ans,
-        'prompt': context_prompt,
-        'reference_info': formatted_reference_info,
-        'answer_token_num': answer_token_num,
-        'prompt_token_num': prompt_token_num,
-    })
+    conversation["history"].append(
+        {
+            "role": "assistant",
+            "content": last_ans,
+            "prompt": context_prompt,
+            "reference_info": formatted_reference_info,
+            "answer_token_num": answer_token_num,
+            "prompt_token_num": prompt_token_num,
+        }
+    )
     print()
 
 
 def print_loading_mark() -> None:
     global is_generating
 
-    loading_mark = ['-', '\\', '|', '/']
+    loading_mark = ["-", "\\", "|", "/"]
     idx = 0
     while True:
         if is_generating:
             ch = loading_mark[idx % len(loading_mark)]
             idx = (idx + 1) % len(loading_mark)
-            print(ch, end='\r', flush=True)
+            print(ch, end="\r", flush=True)
 
         time.sleep(0.05)
 
@@ -134,25 +135,25 @@ def parse_user_instruct(user_input: str) -> None:
     if len(user_input) == 0:
         return
 
-    if user_input in ['?', '/help']:
+    if user_input in ["?", "/help"]:
         print("""Help info:
 /help: show help info.
 /exit: exit and save conversation as json.
 """)
-    elif user_input == '/exit':
+    elif user_input == "/exit":
         os.makedirs(os.path.dirname(config.CONVERSATION_SAVE_PATH), exist_ok=True)
 
-        if len(conversation['history']) >= 2:
-            with open(config.CONVERSATION_SAVE_PATH, 'w+') as f:
+        if len(conversation["history"]) >= 2:
+            with open(config.CONVERSATION_SAVE_PATH, "w+") as f:
                 json.dump(conversation, f, ensure_ascii=False, indent=4)
-                print(f'save conversation to {config.CONVERSATION_SAVE_PATH}')
+                print(f"save conversation to {config.CONVERSATION_SAVE_PATH}")
 
-        print('byte:)')
+        print("byte:)")
         os._exit(0)
 
     else:
         # talk to LLM
-        conversation['history'].append({'role': 'user', 'content': user_input})
+        conversation["history"].append({"role": "user", "content": user_input})
 
         generate_response()
 
@@ -173,14 +174,14 @@ def run_chat() -> None:
             os._exit(0)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     is_generating = False
 
     job_executor = get_job_executor()
     job_executor.submit(print_loading_mark)
 
-    if 'history' not in conversation:
-        conversation['history'] = []
+    if "history" not in conversation:
+        conversation["history"] = []
 
     try:
         run_chat()
